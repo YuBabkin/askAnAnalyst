42edc435e412a479016d41c72ac28330f633f6d8
Yura Babkin <YuVlBabkin@yandex.ru>
2022-06-02 23:58:59 +0300

    Начинаем.

---
+++ analysts_answer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 23:37:45 2022
+
+@author: babkin
+"""
+
+import random
+
+
+def analysts_answer():
+    """ Спроси аналитика
+    """
+
+    list_of_answers = ["Да мне похуй что ты там наговнокодил!",
+                       "Да мне по барабану!",
+                       "Сам разбирася со своим говном.",
+                       "А я ебу, что за хуйню ты тут высрал",
+                       "Ну и с какого комита така ебала завелась?!",
+                       "И чё, бля?"]
+
+    a = random.randint(1, len(list_of_answers)-1 )
+
+    print(list_of_answers[a])
+
+
